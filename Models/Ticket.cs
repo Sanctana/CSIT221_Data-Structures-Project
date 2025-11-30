767019e0ea95cd226ec6369e1cdd4ebfc6d464be
Sanctana <triatangpos@yahoo.com>
2025-11-30 15:51:51 +0800

    Small addition

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -3,6 +3,7 @@ namespace Models;
 public class Ticket
 {
     public int Id { get; set; }
+    public string seatId { get; set; }
     public string? HolderName { get; set; }
     public DateTime IssueTime { get; set; }
 }
