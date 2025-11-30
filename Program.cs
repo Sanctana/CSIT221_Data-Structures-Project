e06d02acb54b187c3e1b72877bb82ef1d5805f62
Sanctana <triatangpos@yahoo.com>
2025-11-30 15:51:46 +0800

    feat: TicketQueueService

--- Program.cs
+++ Program.cs
@@ -1,11 +1,33 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CSIT221_Data_Structures_Project;
+using Services;
+using System.Text.Json;
+using Models;
+using Utilities;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<TicketQueueService>();
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+try
+{
+    var http = host.Services.GetRequiredService<HttpClient>();
+    var ticketService = host.Services.GetRequiredService<TicketQueueService>();
+
+    var json = await http.GetStringAsync("sample-data/tickets.json");
+    var tickets = JsonSerializer.Deserialize<List<Ticket>>(json, JsonOptions.Default);
+
+    ticketService.Initialize(tickets);
+}
+catch
+{
+
+}
+
+await host.RunAsync();
---
+++ Services/TicketQueueService.cs
@@ -0,0 +1,44 @@
+using Models;
+
+namespace Services;
+
+public class TicketQueueService
+{
+    private readonly Queue<Ticket> _queue = new();
+    private bool _initialized = false;
+
+    public void Enqueue(Ticket t)
+    {
+        lock (_queue) { _queue.Enqueue(t); }
+    }
+
+    public Ticket? Dequeue()
+    {
+        lock (_queue) { return _queue.Count > 0 ? _queue.Dequeue() : null; }
+    }
+
+    public IReadOnlyCollection<Ticket> GetAll()
+    {
+        lock (_queue) { return [.. _queue]; }
+    }
+
+    public int Count
+    {
+        get { lock (_queue) { return _queue.Count; } }
+    }
+
+    public void Initialize(IEnumerable<Ticket>? tickets)
+    {
+        if (tickets == null || _initialized) return;
+
+        lock (_queue)
+        {
+            foreach (var t in tickets)
+            {
+                _queue.Enqueue(t);
+            }
+
+        }
+        _initialized = true;
+    }
+}
