72d64353d33edb3d851f6606e2f382e5a1c995cd
KateyCat04 <154388103+KateyCat04@users.noreply.github.com>
2025-11-30 22:59:26 +0800

    Update PassengerSeatService.cs

--- Services/PassengerSeatService.cs
+++ Services/PassengerSeatService.cs
@@ -3,55 +3,63 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Services;
-
-public class PassengerSeatService : IEnumerable<PassengerSeat>
+namespace Services
 {
-    // switched from Utilities.ArrayList to a generic List for ease of use
-    private readonly List<PassengerSeat> _passengerSeats = new();
-
-    public void Initialize(IEnumerable<PassengerSeat>? passengerSeats)
+    public class PassengerSeatService : IEnumerable<PassengerSeat>
     {
-        _passengerSeats.Clear();
-        if (passengerSeats == null) return;
+        // Internal storage
+        private readonly List<PassengerSeat> _passengerSeats = new();
 
-        _passengerSeats.AddRange(passengerSeats);
+        // Initialize / Reset
+        public void Initialize(IEnumerable<PassengerSeat>? passengerSeats)
+        {
+            _passengerSeats.Clear();
+            if (passengerSeats != null)
+                _passengerSeats.AddRange(passengerSeats);
+        }
+
+        // Query by flight
+        public IEnumerable<PassengerSeat> GetPassengerSeatsByFlightId(int flightId) =>
+            _passengerSeats.Where(ps => ps.FlightId == flightId);
+
+        // Add
+        public void AddPassengerSeat(PassengerSeat seat) => _passengerSeats.Add(seat);
+        public void AddLast(PassengerSeat seat) => _passengerSeats.Add(seat);
+
+        // Count
+        public int Count => _passengerSeats.Count;
+
+        // Indexer
+        public PassengerSeat this[int index]
+        {
+            get => _passengerSeats[index];
+            set => _passengerSeats[index] = value;
+        }
+
+        // Remove by index
+        public void RemoveAt(int index) => _passengerSeats.RemoveAt(index);
+
+        // Convert to list
+        public List<PassengerSeat> ToList() => new(_passengerSeats);
+
+        // Find index
+        public int IndexOf(Func<PassengerSeat, bool> predicate) =>
+            _passengerSeats.FindIndex(ps => predicate(ps));
+
+        public void RemoveWhere(Func<PassengerSeat, bool> predicate)
+        {
+            _passengerSeats.RemoveAll(ps => predicate(ps));
+        }
+
+        public void Update(PassengerSeat updatedSeat)
+        {
+            int index = _passengerSeats.FindIndex(ps => ps.Id == updatedSeat.Id);
+
+            if (index >= 0)
+                _passengerSeats[index] = updatedSeat;
+        }
+        
+        public IEnumerator<PassengerSeat> GetEnumerator() => _passengerSeats.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => _passengerSeats.GetEnumerator();
     }
-
-    public IEnumerable<PassengerSeat> GetPassengerSeatsByFlightId(int flightId) =>
-        _passengerSeats.Where(ps => ps.FlightId == flightId);
-
-    IEnumerator<PassengerSeat> IEnumerable<PassengerSeat>.GetEnumerator() => _passengerSeats.GetEnumerator();
-
-    IEnumerator IEnumerable.GetEnumerator() => _passengerSeats.GetEnumerator();
-
-    public void AddPassengerSeat(PassengerSeat passengerSeat)
-    {
-        _passengerSeats.Add(passengerSeat);
-    }
-
-    public int Count => _passengerSeats.Count;
-
-    public void AddLast(PassengerSeat passengerSeat)
-    {
-        _passengerSeats.Add(passengerSeat);
-    }
-
-    public PassengerSeat this[int index]
-    {
-        get => _passengerSeats[index];
-        set => _passengerSeats[index] = value;
-    }
-
-    public void RemoveAt(int index) => _passengerSeats.RemoveAt(index);
-
-    public List<PassengerSeat> ToList() => new List<PassengerSeat>(_passengerSeats);
-
-    public int IndexOf(Func<PassengerSeat, bool> predicate) =>
-        _passengerSeats.FindIndex(ps => predicate(ps));
-}
-
-public void RemoveWhere(Func<PassengerSeat, bool> predicate)
-{
-    _passengerSeats.RemoveAll(ps => predicate(ps));
 }
